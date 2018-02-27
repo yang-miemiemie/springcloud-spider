d14670f1f3ed0ce798011147b883ca4d2a2a5626
yang-miemiemie <a371668144@163.com>
2018-02-27 11:40:49 +0800

    添加开启定时任务注解

--- src/main/java/com/mie/spider/SpringcloudSpiderApplication.java
+++ src/main/java/com/mie/spider/SpringcloudSpiderApplication.java
@@ -6,12 +6,14 @@ import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableEurekaClient
 @EnableFeignClients
 @EnableCircuitBreaker
 @EnableHystrix
+@EnableScheduling
 public class SpringcloudSpiderApplication {
 
 	public static void main(String[] args) {
